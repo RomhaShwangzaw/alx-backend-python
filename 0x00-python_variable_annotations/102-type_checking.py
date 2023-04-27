bace737dfa5cbf96624b2c970f8d33fd6123d5ea
Romha Shwangzaw <romhas@gmail.com>
2023-04-27 15:39:06 +0300

    added documentation to zoom_array() function

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,6 +4,10 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Copies each element in the tuple `lst` by a factor of `factor` and
+    places it in a new list. It finally returns the newly formed list
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
